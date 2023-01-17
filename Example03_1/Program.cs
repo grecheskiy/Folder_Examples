aba03918be5f8dc6c8b5085918ad6c84fb5b627a
grecheskiy <grecheskiy@rambler.ru>
2023-01-18 01:06:51 +0300

    added Example

---
+++ Example03_1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter a number: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int result = number * number;
+Console.WriteLine("Square of number: " + result);
