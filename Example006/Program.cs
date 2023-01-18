05ca68b98187ad47e28870e7631b5e83cf940a16
grecheskiy <grecheskiy@rambler.ru>
2023-01-19 00:48:13 +0300

    Example006

---
+++ Example006/Program.cs
@@ -0,0 +1,7 @@
+﻿//Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int [] numbers = {10, 99};
+int a = numbers[0]; int b = numbers[1];
+if (a > b)
+Console.WriteLine(numbers[0]);
+else
+Console.WriteLine(numbers[1]);
