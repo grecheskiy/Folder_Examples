4494862a2a0d9a12cf87cbcb543d1592cc466a51
grecheskiy <grecheskiy@rambler.ru>
2023-01-19 01:19:14 +0300

    Example007

---
+++ Example007/Program.cs
@@ -0,0 +1,10 @@
+﻿//Удалить вторую цифру трёхзначного числа 
+Console.Write("Enter a three-digit number: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int x1 = n / 100;
+int x2 = (n / 10) % 10;
+int x3 = n % 10;
+if (n>=100)
+Console.WriteLine(x1 + "" + x3);
+else
+Console.WriteLine(" False ");
