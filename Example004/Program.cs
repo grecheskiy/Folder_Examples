883de55fc56c6eff40e6e72e6e74f9fb1e60fc45
grecheskiy <grecheskiy@rambler.ru>
2023-01-19 00:31:39 +0300

    Example003\4

---
+++ Example004/Program.cs
@@ -0,0 +1,8 @@
+﻿//Показать последнюю цифру трёхзначного числа
+Console.Write("Enter a three-digit number: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int x3 = n % 10;
+if (n>=100)
+Console.WriteLine(x3);
+else
+Console.WriteLine(" False ");
