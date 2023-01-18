39d6afcc82e251560f44c34367c40dab02976979
grecheskiy <grecheskiy@rambler.ru>
2023-01-19 01:50:15 +0300

    Example008

---
+++ Example009/Program.cs
@@ -0,0 +1,8 @@
+﻿//Найти третью цифру числа или сообщить, что её нет
+Console.Write("Enter a number: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+string str = n.ToString();
+if (n>=100)
+Console.WriteLine(str[3]);
+else
+Console.WriteLine(" Not found ");
