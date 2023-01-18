b3d51c9b1fc4e5bd9e05824eecc54d8f6666b51d
grecheskiy <grecheskiy@rambler.ru>
2023-01-18 23:35:20 +0300

    Example002

---
+++ Example002/Program.cs
@@ -0,0 +1,17 @@
+﻿//По заданному номеру дня недели вывести его название
+Console.Write("Enter a number 1 to 7: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+    if (n == 1)
+    Console.WriteLine("Monday");
+    if (n == 2)
+    Console.WriteLine("Tuesday");
+    if (n == 3)
+    Console.WriteLine("Wednesday");
+    if (n == 4)
+    Console.WriteLine("Thursday");
+    if (n == 5)
+    Console.WriteLine("Friday");
+    if (n == 6)
+    Console.WriteLine("Saturday");
+    if (n == 7)
+    Console.WriteLine("Sunday"); 
