cc754900cbae192323fbdeb16c5d3d7dc39166f7
grecheskiy <grecheskiy@rambler.ru>
2023-01-19 00:36:30 +0300

    Example005

---
+++ Example005/Program.cs
@@ -0,0 +1,8 @@
+﻿//Показать вторую цифру трёхзначного числа
+Console.Write("Enter a three-digit number: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int x2 = (n/10)%10;
+if (n>=100)
+Console.WriteLine(x2);
+else
+Console.WriteLine(" False ");
