1c408024017b92921afd4bdc8c0b531af8549472
grecheskiy <grecheskiy@rambler.ru>
2023-01-18 23:47:57 +0300

    Example003

---
+++ Example003/Program.cs
@@ -0,0 +1,10 @@
+﻿//Выяснить является ли число чётным
+Console.Write("Enter a number: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int reuslt = n % 2;
+if (reuslt == 0)
+//int number = Convert.ToInt32(Console.ReadLine()) % 2;
+//if (number == 0)
+{Console.WriteLine(" even number ");}
+else
+{Console.WriteLine(" odd number ");}
